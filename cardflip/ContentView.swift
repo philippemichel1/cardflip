e1a37948aa6cf826b794de7aa71102bcfc9702a5
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-20 22:56:50 +0200

    modification orientation

--- cardflip/ContentView.swift
+++ cardflip/ContentView.swift
@@ -1,6 +1,7 @@
+
 //
 //  ContentView.swift
-//  cardflip
+//  CardFlip
 //
 //  Created by Philippe MICHEL on 20/04/2024.
 //
@@ -8,17 +9,98 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var  image:Image?
+    @State private var inputImage:UIImage?
+    @State private var showView:Bool = false
+    @State private var showLibrary:Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            VStack {
+                Text("Retourne Carte")
+                    .font(.largeTitle)
+                    .padding(.top,0)
+                ZStack {
+                    if showView {
+                        image?
+                            .resizable()
+                            .cornerRadius(10)
+                            .transition(.reveseflip)
+                    } else {
+                        Image("dos")
+                            .resizable()
+                            .transition(.flip)
+                    }
+                    
+                }
+                .frame(width:200, height: 300)
+                
+                // pour faire rotation il faut avoir choisit une image
+                if !(image == nil) {
+                    Button(showView ? "Hide" : "Reveal") {
+                        withAnimation(.bouncy(duration: 2)) {
+                            self.showView.toggle()
+                        }
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(width: 200, height: 50)
+                    .foregroundColor(.white)
+                    .background(.green)
+                    .cornerRadius(10)
+                    .padding(.top, 30)
+                }
+                
+                // ouvrir la lib de photo
+                Button("Open Photo Library") {
+                    self.showLibrary.toggle()
+                }
+                .buttonStyle(PlainButtonStyle())
+                .frame(width: 200, height: 50)
+                .foregroundColor(.white)
+                .background(.blue)
+                .cornerRadius(10)
+                .padding()
+                
+                // ouvrir fenetre sheet
+                .sheet(isPresented: $showLibrary) {
+                    PhotoPicker(image: $inputImage)
+                }
+                .onChange(of:inputImage) {loadImage()}
+            }
+    }
+    //Chargement Image
+    func loadImage() {
+        guard let inputImage = inputImage else {return}
+        image = Image(uiImage: inputImage)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct FlipTransition:ViewModifier, Animatable {
+    var progress:CGFloat = 0
+    var animatableData: CGFloat {
+        get {progress}
+        set {progress = newValue }
+    }
+    func body(content: Content) -> some View {
+        content
+            .opacity(progress < 0 ? (-progress < 0.5 ? 1 : 0) : (progress < 0.5 ? 1 : 0))
+            .rotation3DEffect(Angle(degrees: progress * 180),
+                axis: (x: 0.0, y: 1.0, z: 0.0)
+            )
+    }
+}
+
+extension AnyTransition {
+    static let flip: AnyTransition = .modifier(
+        active: FlipTransition(progress: -1),
+        identity: FlipTransition())
+
+
+static let reveseflip: AnyTransition = .modifier(
+    active: FlipTransition(progress: 1),
+    identity: FlipTransition())
+
 }
--- cardflip/ViewModel/PhotoPicker.swift
+++ cardflip/ViewModel/PhotoPicker.swift
@@ -5,4 +5,51 @@
 //  Created by Philippe MICHEL on 20/04/2024.
 //
 
-import Foundation
+import SwiftUI
+import PhotosUI
+
+struct PhotoPicker:UIViewControllerRepresentable {
+    @Binding var image:UIImage?
+    
+    class Coordinator:NSObject, PHPickerViewControllerDelegate {
+        var parent:PhotoPicker
+        init(parent: PhotoPicker) {
+            self.parent = parent
+        }
+        
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else {return}
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+            
+        }
+    }
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    
+}
+
+
