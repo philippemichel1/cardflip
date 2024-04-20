81ecbd878f207fff13805898b30bd7311bc0d40d
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-20 19:26:31 +0200

    Initial Commit

---
+++ cardflip/cardflipApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardflipApp.swift
+//  cardflip
+//
+//  Created by Philippe MICHEL on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct cardflipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
