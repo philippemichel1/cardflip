5695510de90aea5567066bda366747beb529c020
Philippe MICHEL <philippe.michel1@gmail.com>
2024-04-21 13:41:18 +0200

    taper sur image

--- cardflip/ContentView.swift
+++ cardflip/ContentView.swift
@@ -15,57 +15,67 @@ struct ContentView: View {
     @State private var showLibrary:Bool = false
     
     var body: some View {
-            VStack {
-                Text("Retourne Carte")
-                    .font(.largeTitle)
-                    .padding(.top,0)
-                ZStack {
-                    if showView {
-                        image?
-                            .resizable()
-                            .cornerRadius(10)
-                            .transition(.reveseflip)
-                    } else {
-                        Image("dos")
-                            .resizable()
-                            .transition(.flip)
-                    }
-                    
-                }
-                .frame(width:200, height: 300)
-                
-                // pour faire rotation il faut avoir choisit une image
-                if !(image == nil) {
-                    Button(showView ? "Hide" : "Reveal") {
-                        withAnimation(.bouncy(duration: 2)) {
-                            self.showView.toggle()
+        VStack {
+            Text("Retourne Carte")
+                .font(.largeTitle)
+                .padding(.top,0)
+            ZStack {
+                if showView {
+                    image?
+                        .resizable()
+                        .cornerRadius(10)
+                        .transition(.reveseflip)
+                        .onTapGesture {
+                            withAnimation(.bouncy(duration: 2)) {
+                                self.showView.toggle()
+                            }
                         }
-                    }
-                    .buttonStyle(PlainButtonStyle())
-                    .frame(width: 200, height: 50)
-                    .foregroundColor(.white)
-                    .background(.green)
-                    .cornerRadius(10)
-                    .padding(.top, 30)
+                } else {
+                    Image("dos")
+                        .resizable()
+                        .transition(.flip)
+                        .onTapGesture {
+                            withAnimation(.bouncy(duration: 2)) {
+                                self.showView.toggle()
+                            }
+                        }
                 }
                 
-                // ouvrir la lib de photo
-                Button("Open Photo Library") {
-                    self.showLibrary.toggle()
-                }
-                .buttonStyle(PlainButtonStyle())
-                .frame(width: 200, height: 50)
-                .foregroundColor(.white)
-                .background(.blue)
-                .cornerRadius(10)
-                .padding()
-                
-                // ouvrir fenetre sheet
-                .sheet(isPresented: $showLibrary) {
-                    PhotoPicker(image: $inputImage)
-                }
-                .onChange(of:inputImage) {loadImage()}
             }
+            .frame(width:200, height: 300)
+            
+            // pour faire rotation il faut avoir choisit une image
+//            if !(image == nil) {
+//                Button(showView ? "Hide" : "Reveal") {
+//                    withAnimation(.bouncy(duration: 2)) {
+//                        self.showView.toggle()
+//                    }
+//                }
+//                .buttonStyle(PlainButtonStyle())
+//                .frame(width: 200, height: 50)
+//                .foregroundColor(.white)
+//                .background(.green)
+//                .cornerRadius(10)
+//                .padding(.top, 30)
+//            }
+            
+            // ouvrir la lib de photo
+            Button("Open Photo Library") {
+                self.showLibrary.toggle()
+            }
+            .buttonStyle(PlainButtonStyle())
+            .frame(width: 200, height: 50)
+            .foregroundColor(.white)
+            .background(.blue)
+            .cornerRadius(10)
+            .padding()
+            
+            // ouvrir fenetre sheet
+            .sheet(isPresented: $showLibrary) {
+                PhotoPicker(image: $inputImage)
+            }
+            .onChange(of:inputImage) {loadImage()}
+        }
     }
     //Chargement Image
     func loadImage() {
@@ -88,7 +98,7 @@ struct FlipTransition:ViewModifier, Animatable {
         content
             .opacity(progress < 0 ? (-progress < 0.5 ? 1 : 0) : (progress < 0.5 ? 1 : 0))
             .rotation3DEffect(Angle(degrees: progress * 180),
-                axis: (x: 0.0, y: 1.0, z: 0.0)
+                              axis: (x: 0.0, y: 1.0, z: 0.0)
             )
     }
 }
@@ -97,10 +107,10 @@ extension AnyTransition {
     static let flip: AnyTransition = .modifier(
         active: FlipTransition(progress: -1),
         identity: FlipTransition())
-
-
-static let reveseflip: AnyTransition = .modifier(
-    active: FlipTransition(progress: 1),
-    identity: FlipTransition())
-
+    
+    
+    static let reveseflip: AnyTransition = .modifier(
+        active: FlipTransition(progress: 1),
+        identity: FlipTransition())
+    
 }
